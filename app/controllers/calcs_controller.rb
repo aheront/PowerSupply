0b96b57c915b788f8f7338b9dca9edfe2a864e43
aheront <aheronth@gmail.com>
2017-01-25 10:18:02 +0200

    View

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -16,7 +16,7 @@ class CalcsController < ApplicationController
     @vga_count = calc_params[:vga_count].to_i
     @fan_count = calc_params[:fan_count].to_i
     @sata_count = calc_params[:sata_count].to_i
-    @calc = @cpu.power+(@vga.vga_power*@vga_count) +(@ram.power_ram*@ram_count)+(@fan_count*3)+@wat.water_power+@math.power+(@sata_count*15)
+    @calc = @cpu.power + (@vga.vga_power*@vga_count) + (@ram.power_ram * @ram_count) + (@fan_count*3) + @wat.water_power + @math.power + (@sata_count * 25)
   end
   private
   def calc_params
